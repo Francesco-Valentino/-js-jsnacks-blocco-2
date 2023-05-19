d6a7c5ba7e30718742f4ad518673eceb2fbb84dd
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-19 17:56:59 +0200

    snack 2 complete

---
+++ script.js
@@ -0,0 +1,27 @@
+const randomNumber = Math.floor(Math.random() * 100 + 1);
+
+console.log(randomNumber);
+
+let inputNumber;
+
+let playerTries = [];
+
+//devo ripetere la domanda FINCHE' randomNumber è uguale a inputNumber
+while (inputNumber !== randomNumber){
+    if (inputNumber < randomNumber){
+        playerTries.push(inputNumber)
+        console.log("The right number is smaller.")
+    } else if (inputNumber> randomNumber){
+        playerTries.push(inputNumber)
+        console.log("The right number is greater.")
+    }
+
+    inputNumber = parseInt(prompt("Insert a number."))
+}
+
+if (randomNumber === inputNumber && playerTries.length === 0){
+    console.log("You found the number!");
+} else if (inputNumber === randomNumber){
+    console.log("You found the number!")
+    console.log("You have tried " + playerTries.length + " times.")
+} 
